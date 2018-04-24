87de18084d8464ca493bda47c8333d45ae92d044
Aishwarya.Dixit <Aishwarya.Dixit@ASIA18857>
2018-04-24 15:51:56 +0530

    -Added Project Controller

---
+++ OnboardingManagement/Controllers/ProjectController.cs
@@ -0,0 +1,101 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using OnboardingManagement.Models;
+
+namespace OnboardingManagement.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private OnboardingManagementDb db = new OnboardingManagementDb();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Projects_Read([DataSourceRequest]DataSourceRequest request)
+        {
+            IQueryable<Project> projects = db.Projects;
+            DataSourceResult result = projects.ToDataSourceResult(request, project => new {
+                P_Id = project.P_Id,
+                P_Name = project.P_Name,
+                P_Technology = project.P_Technology
+            });
+
+            return Json(result);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Projects_Create([DataSourceRequest]DataSourceRequest request, Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new Project
+                {
+                    P_Name = project.P_Name,
+                    P_Technology = project.P_Technology
+                };
+
+                db.Projects.Add(entity);
+                db.SaveChanges();
+                project.P_Id = entity.P_Id;
+            }
+
+            return Json(new[] { project }.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Projects_Update([DataSourceRequest]DataSourceRequest request, Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new Project
+                {
+                    P_Id = project.P_Id,
+                    P_Name = project.P_Name,
+                    P_Technology = project.P_Technology
+                };
+
+                db.Projects.Attach(entity);
+                db.Entry(entity).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            return Json(new[] { project }.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Projects_Destroy([DataSourceRequest]DataSourceRequest request, Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new Project
+                {
+                    P_Id = project.P_Id,
+                    P_Name = project.P_Name,
+                    P_Technology = project.P_Technology
+                };
+
+                db.Projects.Attach(entity);
+                db.Projects.Remove(entity);
+                db.SaveChanges();
+            }
+
+            return Json(new[] { project }.ToDataSourceResult(request, ModelState));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
