ff10347b2d554192d3f5770d8a82853f0f2bcdd6
Amrut.Pardeshi <Amrut.Pardeshi@ASIA17028>
2018-04-26 15:11:09 +0530

    -Project Assignment Controller

--- OnboardingManagement/Controllers/ProjectAssignmentController.cs
+++ OnboardingManagement/Controllers/ProjectAssignmentController.cs
@@ -68,6 +68,7 @@ namespace OnboardingManagement.Controllers
             IQueryable<ProjectAssignment> projectassignemts = db.ProjectAssignments;
             DataSourceResult result = projectassignemts.ToDataSourceResult(request, projectassignemt => new
             {
+                PA_Id = projectassignemt.PA_Id,
                 O_Id = projectassignemt.O_Id,
                 P_Id = projectassignemt.P_Id,
                 M_Id = projectassignemt.M_Id,
@@ -78,5 +79,63 @@ namespace OnboardingManagement.Controllers
 
             return Json(result);
         }
+
+
+        public ActionResult ProjectAssignment_Destroy([DataSourceRequest]DataSourceRequest request, ProjectAssignment projectAssignment)
+        {
+           
+                var entity = new ProjectAssignment
+                {
+                    PA_Id = projectAssignment.PA_Id,
+                    O_Id = projectAssignment.O_Id,
+                    P_Id= projectAssignment.P_Id,
+                    M_Id = projectAssignment.M_Id,
+                    PA_Rotation_Num = projectAssignment.PA_Rotation_Num,
+                    PA_Start_Date = projectAssignment.PA_Start_Date,
+                    PA_End_Date = projectAssignment.PA_End_Date
+
+                };
+
+                db.ProjectAssignments.Attach(entity);
+                db.ProjectAssignments.Remove(entity);
+                db.SaveChanges();
+            
+
+            return Json(new[] { projectAssignment }.ToDataSourceResult(request, ModelState));
+        }
+
+        public ActionResult ProjectAssignment_Delete()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult ProjectAssignment_Delete_Fetch(string o_id,string rotation_no)
+        {
+            int id1 = Int32.Parse(o_id), rotation = Int32.Parse(rotation_no);
+            ProjectAssignment projectAssignmet = db.ProjectAssignments.FirstOrDefault(m => m.O_Id == id1 &&  m.PA_Rotation_Num == rotation);
+            TempProjectAssignment tpa = new TempProjectAssignment
+            {
+                PA_Id = id1,
+                ProjectName = db.Projects.Find(projectAssignmet.P_Id).P_Name,
+                OnboarderName= db.Onboarders.Find(projectAssignmet.P_Id).O_Name,
+                RotationNo= projectAssignmet.PA_Rotation_Num,
+                MentorName = db.Mentors.Find(projectAssignmet.P_Id).M_Name,
+                StartData = projectAssignmet.PA_Start_Date,
+                EndData= projectAssignmet.PA_Start_Date
+            };
+            return Json(tpa, JsonRequestBehavior.AllowGet);
+        }
+
     }
+    class TempProjectAssignment
+    {
+        public int PA_Id { get; set; }
+        public string ProjectName { get; set; }
+        public string OnboarderName { get; set; }
+        public int RotationNo { get; set; }
+        public string MentorName { get; set; }
+        public DateTime StartData { get; set; }
+        public DateTime EndData { get; set; }
+    };
 }
