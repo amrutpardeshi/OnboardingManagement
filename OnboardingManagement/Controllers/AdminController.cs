5c9a04717139128b3fda5a19a747d4f55502ced2
Darshan.Jain <Darshan.Jain@ASIA18861>
2018-04-27 14:48:42 +0530

    -Some bugs in 3 table join is resolved. -Modified the logic of getting all onboarder who work under Logged In mentor -Mentor Homepage After Login In  Designing and data of all onborder showed in grid View

--- OnboardingManagement/Controllers/AdminController.cs
+++ OnboardingManagement/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using OnboardingManagement.Models;
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using OnboardingManagement.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,8 +14,9 @@ namespace OnboardingManagement.Controllers
         OnboardingManagementDb db = new OnboardingManagementDb();
         [Authorize(Roles ="Admin")]
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Index(int id)
         {
+            ViewBag.id = id;
             return View();
         }
 
@@ -40,55 +43,55 @@ namespace OnboardingManagement.Controllers
             return db.Onboarders.FirstOrDefault(m => m.O_Id == OnboarderId).O_Name;
         }
 
-        [Authorize]
-        public ActionResult MentorView(int id)
+        public JsonResult GetOnboardersForMentor([DataSourceRequest]DataSourceRequest request, int id)
         {
-
+            
+            String MentorName = db.Mentors.FirstOrDefault(m => m.M_Id == id).M_Name;
+            ViewBag.MentorName = MentorName;
 
             DateTime today = DateTime.Now;
-
-           List<ProjectAssignment> projects= db.ProjectAssignments
-                                                .Where(m => m.PA_Start_Date < today && m.PA_End_Date > today && m.M_Id == id).ToList(); //Get All Onboarder
-
-
-
+            List<ProjectAssignment> projects = db.ProjectAssignments
+                                                 .Where(m => m.PA_Start_Date < today && m.PA_End_Date > today && m.M_Id == id).ToList(); //Get All Onboarder
             List<OnBoarderDetailModel> onboarderslist = new List<OnBoarderDetailModel>();
-         
             foreach (ProjectAssignment project in projects)
             {
                 OnBoarderDetailModel model = new OnBoarderDetailModel();
-
                 List<ProjectAssignment> onboarderProject = db.ProjectAssignments.Where(m => m.O_Id == project.O_Id).ToList();//get all projects of onboarder
-                
-                    foreach(ProjectAssignment onboarderdetail in onboarderProject)
+                foreach (ProjectAssignment onboarderdetail in onboarderProject)
+                {
+                    model.OnboarderId = onboarderdetail.O_Id;
+                    model.Name = GetOnboarderNameById(onboarderdetail.O_Id);
+                    String projectName = GetProjectNameById(onboarderdetail.P_Id);
+                    if (model.Rotation1.Equals("---"))
                     {
-                            model.Name = GetOnboarderNameById(onboarderdetail.O_Id);
-                            String projectName = GetProjectNameById(onboarderdetail.P_Id);
-                            if (model.Rotation1.Equals("---")) {
-                                model.Rotation1 = projectName;
-                            }
-                            else if (model.Rotation2.Equals("---"))
-                            {
-                                model.Rotation2 = projectName;
-                            }
-                            else if (model.Rotation3.Equals("---"))
-                            {
-                                model.Rotation3 = projectName;
-                            }
-                            else if (model.Rotation4.Equals("---"))
-                            {
-                                model.Rotation4 = projectName;
-                            }
+                        model.Rotation1 = projectName;
+                    }
+                    else if (model.Rotation2.Equals("---"))
+                    {
+                        model.Rotation2 = projectName;
+                    }
+                    else if (model.Rotation3.Equals("---"))
+                    {
+                        model.Rotation3 = projectName;
+                    }
+                    else if (model.Rotation4.Equals("---"))
+                    {
+                        model.Rotation4 = projectName;
+                    }
                 }
-
-
                 onboarderslist.Add(model);
-
-
             }
+            return Json(onboarderslist.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
 
 
-            return View(onboarderslist);
+        [Authorize]
+        public ActionResult MentorView()
+        {
+            
+
+
+            return View();
         }
 
     }
